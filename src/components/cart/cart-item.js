0442a24458ca4b8b57818021cecc45173ead24cd
krsahas <krsahas.git@gmail.com>
2019-05-18 01:48:21 +0530

    some css fix

--- src/components/cart/cart-item.js
+++ src/components/cart/cart-item.js
@@ -6,16 +6,16 @@ export default function CartItem({item, value}) {
     return(
         <div>
             <div className="row my-2 text-capitalize text-center">
-                <div className="col-10 mx-auto col-lg-2">
+                <div className="col-10 mx-auto col-lg-1">
                     <img src={img} style={{width:'5rem',height:'5rem'}} className="img-fluid" alt="product" />
                 </div>
-                <div className="col-10 mx-auto col-lg-2 mt-2">
+                <div className="col-10 mx-auto col-lg-1 mt-2">
                     <span className="d-lg-none">product : </span>{title}
                 </div>
-                <div className="col-10 mx-auto col-lg-2 mt-2">
+                <div className="col-10 mx-auto col-lg-1 mt-2">
                     <span className="d-lg-none">price : </span>Rs. {price}
                 </div>
-                <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+                <div className="col-10 mx-auto col-lg-1 my-2 my-lg-0">
                     <div className="d-flex justify-content-center">
                         <div>
                             <span className="btn btn-black mx-1" onClick={()=>decrementId(id)}>- </span>
@@ -26,10 +26,10 @@ export default function CartItem({item, value}) {
                         </div>
                     </div>
                 </div>
-                <div className="col-10 mx-auto col-lg-2 cart-icon">
+                <div className="col-10 mx-auto col-lg-1 cart-icon">
                     <i className="fa fa-times" onClick={()=>removeItem(id)} />
                 </div>
-                <div className="col-10 mx-auto col-lg-2 mt-2">
+                <div className="col-10 mx-auto col-lg-1 mt-2">
                 <strong>price : Rs. {total}</strong>
                 </div>
             </div>

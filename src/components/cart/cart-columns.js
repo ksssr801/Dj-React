6944af121925b1b545b05fc89e81b569f949fbbe
krsahas <krsahas.git@gmail.com>
2019-05-18 01:43:56 +0530

    some fix

--- src/components/cart/cart-columns.js
+++ src/components/cart/cart-columns.js
@@ -4,22 +4,22 @@ export default function CartColumns() {
     return(
         <div className="container-fluid text-centerd d-none d-lg-block">
             <div className="row">
-                <div className="col-10 mx-auto col-lg-2 mt-1">
+                <div className="col-10 mx-auto col-lg-1 mt-1">
                     <p className="text-uppercase text-center">products</p>
                 </div>
-                <div className="col-10 mx-auto col-lg-2 mt-1">
+                <div className="col-10 mx-auto col-lg-1 mt-1">
                     <p className="text-uppercase text-center">name of product</p>
                 </div>
-                <div className="col-10 mx-auto col-lg-2 mt-1">
+                <div className="col-10 mx-auto col-lg-1 mt-1">
                     <p className="text-uppercase text-center">price</p>
                 </div>
-                <div className="col-10 mx-auto col-lg-2 mt-1">
+                <div className="col-10 mx-auto col-lg-1 mt-1">
                     <p className="text-uppercase text-center">quantity</p>
                 </div>
-                <div className="col-10 mx-auto col-lg-2 mt-1">
+                <div className="col-10 mx-auto col-lg-1 mt-1">
                     <p className="text-uppercase text-center">remove</p>
                 </div>
-                <div className="col-10 mx-auto col-lg-2 mt-1">
+                <div className="col-10 mx-auto col-lg-1 mt-1">
                     <p className="text-uppercase text-center">total</p>
                 </div>
             </div>
